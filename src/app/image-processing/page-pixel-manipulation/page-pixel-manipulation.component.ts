5a43a849fc6e5f8428357f495327c0c1878b2aaf
Pierre Roux <pierre.roux6@gmail.com>
2019-04-19 19:54:43 +0200

    Add pixel manipulation page

--- src/app/image-processing/page-pixel-manipulation/page-pixel-manipulation.component.ts
+++ src/app/image-processing/page-pixel-manipulation/page-pixel-manipulation.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { getArrayRange, getRandomInteger } from 'src/app/common/utils';
 
 @Component({
-  selector: 'app-page-pixel-manipulation',
-  templateUrl: './page-pixel-manipulation.component.html',
-  styleUrls: ['./page-pixel-manipulation.component.scss']
-})
+             selector: 'app-page-pixel-manipulation',
+             templateUrl: './page-pixel-manipulation.component.html',
+             styleUrls: ['./page-pixel-manipulation.component.scss']
+           })
 export class PagePixelManipulationComponent implements OnInit {
+
+  colorDepthAmount = 7;
 
   sourcePixels = getArrayRange(8 * 8) // TODO: give real values
     .map(_ => getRandomInteger(7));
+
+  resultPixels = this.sourcePixels // TODO: give real values
+                     .map(i => this.colorDepthAmount - i);
 
   constructor() {
 
